33c82306a06ce3bf4cf9d4ff3ef1d4857b3229f6
Adddefokun Ahira Justice <justiceahira@gmail.com>
2018-07-02 02:48:26 +0100

    Initializing

--- roundrobin.py
+++ roundrobin.py
@@ -4,6 +4,42 @@
 """
 
 
+import os
+import sys
+
+
+def readFile(filename):
+    assert os.path.exists(filename), 'Cannot find the file: %s' % (filename)
+    file = open(filename, 'r')
+    content = []
+    for line in file:
+        newLine = line.split()
+        content.append(newLine)
+    file.close()
+    return content
+
+
+def writeToFile(output, name):
+    file = open(name, 'w')
+    file.write(output)
+    file.close()
+    sys.exit()
+
+
+def writeToOutput(output, out_print):
+    for item in output:
+        item = str(item)
+        out_print += item + '\t'
+    out_print += '\n'
+
+    return out_print
+
+
+def main():
+    processes = readFile('process.txt')
+    print (processes)
+    quantum = processes[0]
+
 
 
 if __name__ == '__main__':
